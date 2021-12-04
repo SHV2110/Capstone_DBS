e0215b155ab3c5f63f4b5afa7f43247189767eec
SHV2110 <49692046+SHV2110@users.noreply.github.com>
2021-12-04 10:09:47 +0000

    mariadb commit

---
+++ custodian.sql
@@ -0,0 +1,7 @@
+insert into custodian(custodian_id,custodian_name)
+values("CS001","BNP Paribas Securities ServicesFrance"),
+("CS002","The Bank of New York Mellon CorporationU.S."),
+("CS003","EuroclearBelgium"),
+("CS004","HSBC Securities ServicesUnited Kingdom"),
+("CS005","Credit SuisseSwitzerland"),
+("CS006","Northern Trust CorporationU.S")
---
+++ instrument.sql
@@ -0,0 +1,19 @@
+insert into instrument (instrument_id,instrument_name,face_value,expiry_date,min_quantity)
+values
+("I001","US treasury Bills",100,"00:00:00.000000",25),
+
+
+
+("I002","RBI-2026 -6.5%",1000,"00:00:00.000000",25),
+
+
+
+("I003","RBI-2045 -8%",1000,"00:00:00.000000",25),
+
+
+
+("I004","USDINR",0,"00:00:00.000000",25),
+
+
+
+("I005","EUR USD-FUT",0,"2021-12-28 23:59:59.999999",25)
