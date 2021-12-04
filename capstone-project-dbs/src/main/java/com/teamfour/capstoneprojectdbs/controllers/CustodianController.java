f0812b0a73debcd52c21cda69146e25f95e4a7e0
SHV2110 <49692046+SHV2110@users.noreply.github.com>
2021-12-04 11:36:50 +0000

    Log Commit

--- capstone-project-dbs/src/main/java/com/teamfour/capstoneprojectdbs/controllers/CustodianController.java
+++ capstone-project-dbs/src/main/java/com/teamfour/capstoneprojectdbs/controllers/CustodianController.java
@@ -1,6 +1,9 @@
 package com.teamfour.capstoneprojectdbs.controllers;
 
 import lombok.AllArgsConstructor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.teamfour.capstoneprojectdbs.beans.Custodian;
-import com.teamfour.capstoneprojectdbs.beans.Instrument;
 import com.teamfour.capstoneprojectdbs.services.CustodianService;
 
 import java.util.List;
@@ -19,9 +21,10 @@ public class CustodianController {
 
     @Autowired
     CustodianService custodianService;
-
+    private static final Logger LOG = LoggerFactory.getLogger(CustodianController.class);
     @GetMapping(value="custodians")
     public ResponseEntity<List<Custodian>> getAllCustodians(){
+    	LOG.info("Get All Custodians");
         return new ResponseEntity<>(custodianService.findAll(), HttpStatus.OK);
     }
 }
